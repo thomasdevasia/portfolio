c031d9485bd3f86ae2bbbef34d1e38512453c2bd
thomas devasia <tdevasia1997@gmail.com>
2022-07-15 02:45:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './styles/index.scss'
+import Header from './components/Header.jsx'
+
 function App() {
   return (
     <div className="App">
-      <h1>
-        hello world
-      </h1>
+      
+      <Header />
+
+      <div>
+        <h4>
+          Hi, my name is
+        </h4>
+        <h2>
+          Thomas Devasia
+        </h2>
+        <h5>i do bla bla something (not heading but bit bold)</h5> 
+
+        <p>
+          maybe a small para? or should be abouy the same?
+        </p>
+      </div>
     </div>
   );
 }
