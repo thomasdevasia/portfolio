14ac46019fdb779eef5c20965d8fbc93e8352f0f
Thomas Devasia <tdevasia1997@gmail.com>
2022-07-16 20:35:44 +0100

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
-import logo from '../images/logo1.svg';
+import logo from '../images/signature-dark.png';
 
 
 const Header = () => {
 
     return(
         <div className='Header'>
-            <div className='navbar'>
-                
-                <img src={logo} className="navbar__logo" alt="logo" />
-                <div className='navbar__list'>
-                    <ul>
-                        <li>About</li>
-                        <li>Experience</li>
-                        <li>Portfolio</li>
-                        <li>Blog</li>
-                        <li>resume ?</li>
-                    </ul>
-                </div>
+
+            <img src={logo} className="header__logo" alt="logo" />
+
+            <div className='navbar'>    
+
+                <ul>
+                    <li>About</li>
+                    <li>Experience</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                </ul>
+
             </div>
+            
+            <a className='button' href='#resume'>Resume</a>
 
         </div>
     )
