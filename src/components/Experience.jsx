039eeee777e7dddd5a23926f394a9eeb4b836d79
Thomas Devasia <tdevasia1997@gmail.com>
2022-07-22 03:18:59 +0100

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,33 @@ const Experience = () => {
     return(
         <div className='experience section'>
             <div className='section__heading'>Experience</div>
+            <div className='section__inner'>
+                <div className='tablist'>
+                    <button className='select'>ABC</button>
+                    <button>DEF</button>
+                    <button>GHI</button>
+                    <button>GHI</button>
+                    <button>GHI</button>
+                </div>
+                <div className='jobs'>
+                    <div className='job'>
+                        <div className='h3'>
+                            Software Engineer <span>@ ABC</span>
+                        </div>
+                        <div className='h5'>
+                            <span>Jan 2019 - Present</span>
+                        </div>
+                        <ul className='list'>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eum ut ipsam ducimus fugit suscipit doloribus animi commodi cumque ratione earum obcaecati deleniti, quas consequuntur! Praesentium, ipsam! Illum, reiciendis est.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eum ut ipsam ducimus fugit suscipit doloribus animi commodi cumque ratione earum obcaecati deleniti, quas consequuntur! Praesentium, ipsam! Illum, reiciendis est.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eum ut ipsam ducimus fugit suscipit doloribus animi commodi cumque ratione earum obcaecati deleniti, quas consequuntur! Praesentium, ipsam! Illum, reiciendis est.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eum ut ipsam ducimus fugit suscipit doloribus animi commodi cumque ratione earum obcaecati deleniti, quas consequuntur! Praesentium, ipsam! Illum, reiciendis est.</li>
+                        </ul>
+                    </div>
+                    <div className='job'></div>
+                    <div className='job'></div>
+                </div>
+            </div>
             
         </div>
     )
