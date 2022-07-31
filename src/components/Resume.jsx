174c3c432e5e823a3285a94faa06274a29f37cc6
Thomas Devasia <tdevasia1997@gmail.com>
2022-07-31 22:32:36 +0100

    update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,34 @@
 import { useRef, useEffect } from 'react';
 import ResumeImg from '../images/resume.png';
 
+// document.getElementById('resume').addEventListener('scroll', (e) => {
+//     console.log('clicked');
+// })
+
 const Resume = () => {
 
     const resumeRef = useRef();
+
+    let style = {
+        color:'#fff',
+    }
+
+    const handleScroll = (e) => {
+        // console.log('scrolled');
+        const top = resumeRef.current.offsetTop;
+        // console.log(top, window.scrollY);
+        if (window.scrollY >= (top - 100) && window.scrollY <= (top + 100)) {
+            console.log('reached');
+        }
+    }
     
     useEffect(() => {
+
+        if(resumeRef.current) {
+            window.addEventListener('scroll', (e) => handleScroll())
+        }
+
+
         const ObserverOptions = {
             threshold: 0.3
         }
@@ -19,7 +42,7 @@ const Resume = () => {
     }, [])
 
     return(
-        <div className="section" ref={resumeRef}>
+        <div className="section initialState" id='resume' ref={resumeRef} style={style} onScroll={handleScroll}>
             <div className="section__heading-center">Souvenier(add scroll effect)</div>
             <div className='resume'>
                 <img src={ResumeImg} alt="resume" />
