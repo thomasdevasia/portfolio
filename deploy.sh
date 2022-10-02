c178b620004a7414d3b487d9504bed761fb68376
Thomas Devasia <80457504+thomasdevasia@users.noreply.github.com>
2022-10-02 03:18:09 +0100

    update to deploy script
    adding environment variable

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ echo "Building the project..."
 npm run build
 
 echo "Deploying the project to server..."
-scp -r build/* tom@109.74.198.112:/var/www/109.74.198.112/
+# save the server address and user as environment
+scp -r build/* $USER@$SERVER_ADDRESS
