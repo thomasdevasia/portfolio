9148f7c7bb4a024a20a29d549673f0d0194f3298
Michael <mdevasia1997@gmail.com>
2023-05-25 15:46:18 +0100

    intro change

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,25 +1,25 @@
 // import Profile from '../images/profile.jpg'
 
 const Intro = () => {
- return(
-    <div className='intro' id="intro">
-        <div className='intro__info slidein slidein-12'>
-            <h4 className='font-bodoni'>
-                Hi, my name is 
-            </h4>
-            <h2>
-                Thomas Devasia
-            </h2>
-            <h5>I am a Software Engineer specializing in Data Science and building Web Apps.</h5> 
+    return (
+        <div className='intro' id="intro">
+            <div className='intro__info slidein slidein-12'>
+                <h4 className='font-bodoni'>
+                    Hi, my name is
+                </h4>
+                <h2>
+                    Thomas Devasia
+                </h2>
+                <h5>I am a Software Engineer specializing in building Web Apps, automation tools, and Data Science.</h5>
 
-            {/* <p>
+                {/* <p>
                 maybe a small para? or should be intro the same?
             </p> */}
-        </div>
+            </div>
 
-        {/* <img className='intro__image' src={Profile} alt="profile" /> */}
-    </div>
- )
+            {/* <img className='intro__image' src={Profile} alt="profile" /> */}
+        </div>
+    )
 }
 
 export default Intro;
